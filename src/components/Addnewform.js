055cc5562b938c6db6e5c8434068858907711fbc
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-02 15:42:11 +0000

    finished add new layout

--- src/components/Addnewform.js
+++ src/components/Addnewform.js
@@ -9,6 +9,19 @@ export default function Addnewform() {
               <div className='addnewform-title'>ADD NEW</div>
               <div className='close-button'>X</div>
             </div>
+            <div className='addnewtext'>ADD A NEW TRANSACTION</div>
+            <div className='text-field-grid'>
+              <input type='text' placeholder='Date'/>
+              <input type='text' placeholder='Date'/>
+              <input type='text' placeholder='Date'/>
+              <input type='text' placeholder='Date'/>
+              <input type='text' placeholder='Date'/>
+              <input type='text' placeholder='Date'/>
+            </div>
+            <div className='canceldonerow'>
+              <div className='doneButton'>Done</div>
+              <div className='cancelButton'>Cancel</div>
+            </div>
         </div>
     </div>
   )
