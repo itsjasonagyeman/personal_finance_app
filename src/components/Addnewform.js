33cf102455f219d2a7b3f6019f361d78f53d8fbc
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-02 16:13:17 +0000

    Finished Add new ui

--- src/components/Addnewform.js
+++ src/components/Addnewform.js
@@ -11,12 +11,25 @@ export default function Addnewform() {
             </div>
             <div className='addnewtext'>ADD A NEW TRANSACTION</div>
             <div className='text-field-grid'>
-              <input type='text' placeholder='Date'/>
-              <input type='text' placeholder='Date'/>
-              <input type='text' placeholder='Date'/>
-              <input type='text' placeholder='Date'/>
-              <input type='text' placeholder='Date'/>
-              <input type='text' placeholder='Date'/>
+              <input type='date' placeholder='Date'/>
+              <input type='text' placeholder='Amount'/>
+              <input type='text' placeholder='Payment Name'/>
+              <select name='method' defaultValue='' required>
+                <option value='' disabled selected >Method</option>
+                <option value='momo'>MOMO</option>
+                <option value='visa'>VISA</option>
+                <option value='other'>Other</option>
+              </select>
+              <select name='category' defaultValue='' required>
+                <option value='' disabled selected>Category</option>
+                <option value='inp'>Income</option>
+                <option value='exp'>Expense</option>
+              </select>
+              <select name='status' defaultValue='' required>
+                <option value='' disabled selected>Status</option>
+                <option value='succ'>Successful</option>
+                <option value='unsucc'>Unsuccessful</option>
+              </select>
             </div>
             <div className='canceldonerow'>
               <div className='doneButton'>Done</div>
