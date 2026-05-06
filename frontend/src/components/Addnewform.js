5edd912ebcdfbb2595877824de51288195f65d36
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-06 12:09:34 +0000

    Removed redundant useState

--- frontend/src/components/Addnewform.js
+++ frontend/src/components/Addnewform.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './Addnewform.css'
 import { authFetch } from '../authfetch';
 
--- frontend/src/components/Transactionheader.js
+++ frontend/src/components/Transactionheader.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Transactionheader.css'
 
 export default function Transactionheader({backgroundColor, showShadow, date, amount, name, method, category, status, activateDelete, setShowPopup, onDeleteClick}) {
